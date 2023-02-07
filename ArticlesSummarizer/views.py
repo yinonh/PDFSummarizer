717e8e4b171ace6e564f891e91dff73207cc90b6
yinonh <yinon.h21@gmail.com>
2023-02-07 12:55:01 +0200

    preparation before deploy

--- ArticlesSummarizer/views.py
+++ ArticlesSummarizer/views.py
@@ -16,52 +16,52 @@ if os.path.isfile(dotenv_file):
 
 def summarize():
     pdf_text = ''
-    # try:
+    try:
         # Open the pdf file in binary mode
-    with open("files/file.pdf", "rb") as file:
-        # Create pdf reader object
-        pdf_reader = PyPDF2.PdfReader(file)
-        # Get the document info
-        pdf_info = pdf_reader.metadata
-        # Get the title from the info
+        with open("files/file.pdf", "rb") as file:
+            # Create pdf reader object
+            pdf_reader = PyPDF2.PdfReader(file)
+            # Get the document info
+            pdf_info = pdf_reader.metadata
+            # Get the title from the info
 
-        # Iterate through each page
-        for page in range(len(pdf_reader.pages)):
-            # Get the page object
-            pdf_page = pdf_reader.pages[page]
-            # Extract the text from the page
-            text = pdf_page.extract_text()
-            # Print the text
-            pdf_text += text
+            # Iterate through each page
+            for page in range(len(pdf_reader.pages)):
+                # Get the page object
+                pdf_page = pdf_reader.pages[page]
+                # Extract the text from the page
+                text = pdf_page.extract_text()
+                # Print the text
+                pdf_text += text
 
-    match = re.findall("\n[1-9 \.]*[A-Z][a-z]*\n", pdf_text)
+        match = re.findall("\n[1-9 \.]*[A-Z][a-z]*\n", pdf_text)
 
-    pdf_dict = {}
-    if match:
-        pdf_dict[match[0]] = pdf_text[:pdf_text.find(match[0])]
-    else:
-        pdf_dict['\nAll the text\n'] = pdf_text
+        pdf_dict = {}
+        if match:
+            pdf_dict[match[0]] = pdf_text[:pdf_text.find(match[0])]
+        else:
+            pdf_dict['\nAll the text\n'] = pdf_text
 
-    for i in range(len(match) - 1):
-        start = pdf_text.find(match[i])
-        end = pdf_text.find(match[i + 1])
-        pdf_dict[match[i]] = pdf_text[start:end]
+        for i in range(len(match) - 1):
+            start = pdf_text.find(match[i])
+            end = pdf_text.find(match[i + 1])
+            pdf_dict[match[i]] = pdf_text[start:end]
 
-    openai.api_key = os.environ['OPENAI_SECRET_KEY']
-    result = '' if pdf_info.title is None else pdf_info.title
-    for i in pdf_dict:
-        try:
-            resopnse = openai.Completion.create(
-                model="text-davinci-003",
-                prompt="summarize this:" + pdf_dict[i][:4096],
-                temperature=0.2,
-                max_tokens=100,
-            )["choices"][0]["text"]
-            result += i + resopnse
-        except TypeError:
-            continue
-    # except:
-    #     return "Error accrued"
+        openai.api_key = os.environ['OPENAI_SECRET_KEY']
+        result = '' if pdf_info.title is None else pdf_info.title
+        for i in pdf_dict:
+            try:
+                resopnse = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt="summarize this:" + pdf_dict[i][:4096],
+                    temperature=0.2,
+                    max_tokens=100,
+                )["choices"][0]["text"]
+                result += i + resopnse
+            except TypeError:
+                continue
+    except:
+        return "Error accrued"
 
     return result
 
@@ -92,8 +92,11 @@ def mainFunc(request):
         else:
             validFile = downloadFile(request.POST.get('textInput'))
         if validFile:
-            s = summarize()
-            os.remove("files/file.pdf")
+            try:
+                s = summarize()
+                os.remove("files/file.pdf")
+            except:
+                s = "Error"
         else:
             return "File invalid"
     return render(request, 'homePage.html', {'summarize': s})
